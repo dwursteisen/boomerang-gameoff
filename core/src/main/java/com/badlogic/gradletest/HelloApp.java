b9bd419e1044c3980c4c592fcc8059e1a982ebe7
David <david.wursteisen+git@gmail.com>
2017-08-05 19:33:49 +0200

    Update to last LibGDX version

--- core/src/main/java/com/badlogic/gradletest/HelloApp.java
+++ core/src/main/java/com/badlogic/gradletest/HelloApp.java
@@ -3,7 +3,7 @@ package com.badlogic.gradletest;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL10;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
@@ -27,7 +27,7 @@ public class HelloApp extends ApplicationAdapter {
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(img, 0, 0);
 		batch.end();
