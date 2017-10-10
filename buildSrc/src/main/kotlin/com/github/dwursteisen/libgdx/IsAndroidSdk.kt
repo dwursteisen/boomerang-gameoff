7c55659bc7f71643004ecc2898079e439b2ec251
David <david.wursteisen+git@gmail.com>
2017-10-10 20:36:27 +0200

    Update IsAndroidSdk

--- buildSrc/src/main/kotlin/com/github/dwursteisen/libgdx/IsAndroidSdk.kt
+++ buildSrc/src/main/kotlin/com/github/dwursteisen/libgdx/IsAndroidSdk.kt
@@ -5,9 +5,43 @@ import java.util.*
 
 object IsAndroidSdk {
 
+    /**
+     * <p>
+     * Force the detection result.
+     *
+     * If you want to the detection to be always true no matter what :
+     *
+     * <pre class="code"><code>
+     *      IsAndroidSdk.forceDetectionTo = true
+     *      // will always be true
+     *      if(IsAndroidSdk.isDetected) {
+     *
+     *      }
+     * </code></pre>
+     * </p>
+     */
     @JvmStatic
-    fun isDetected(): Boolean {
+    var forceDetectionTo: Boolean? = null
 
+    /**
+     * Try to detect if the Android SDK is installed.
+     *
+     * Let you add/remove android specific project when
+     * you haven't installed the Android SDK
+     *
+     * <pre class="code"><code>
+     *      if(IsAndroidSdk.isDetected) {
+     *          // add android plugin...
+     *          apply plugins: 'android'
+     *      }
+     * </code></pre>
+     *
+     */
+    @JvmStatic
+    fun isDetected(): Boolean = forceDetectionTo ?: tryToDetectAndroid()
+
+
+    private fun tryToDetectAndroid(): Boolean {
         val androidHome = System.getenv()["ANDROID_HOME"] != null && System.getenv()["ANDROID_HOME"] != ""
         val localfile = File("local.properties")
         if (localfile.isFile) {
@@ -18,7 +52,6 @@ object IsAndroidSdk {
             return androidHome || androidProperties.getProperty("sdk.dir") != null
 
         }
-
         return androidHome
     }
 
